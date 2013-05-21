27d45e69b76090cd31e1626470221de2b4645f6c
Michael Myers <blackavr@gmail.com>
2013-05-21 15:26:49 -0700

    Implementation for set_ipv4_address

--- providers/network_adapter.rb
+++ providers/network_adapter.rb
@@ -1,5 +1,25 @@
 include Chef::Mixin::ShellOut
+include Windows::Helper
+
+def whyrun_supported?
+  true
+end
+
 
 action :set_ipv4_address do
+  Chef::Log.info 'Setting IPv4 address'
+  converge_by("Shelling out to set IPv4 address") do
+    netsh_command = "#{netsh} interface ipv4 set address #{@new_resource.adapter_name}" +
+        " static #{@new_resource.ipv4_address} #{@new_resource.ipv4_netmask} #{@new_resource.ipv4_gateway}"
+    Chef::Log.info "netsh command is #{netsh_command}"
+    #shell_out!(netsh_command)
+  end
+end
 
+private
+
+def netsh
+  @netsh ||= begin
+    locate_sysnative_cmd("netsh.exe")
+  end
 end
--- resources/network_adapter.rb
+++ resources/network_adapter.rb
@@ -1,7 +1,12 @@
 require 'resolv'
 
 actions :set_ipv4_address
+default_action :nothing
 
-attribute :ipv4_address, :name_attribute => true, :kind_of => String,
+attribute :ipv4_address, :kind_of => String,
           :required => true, :regex => Resolv::IPv4::Regex
-attribute :adapter_name, :kind_of => String, :required => true
+attribute :ipv4_netmask, :kind_of => String, :required => true,
+          :regex => Resolv::IPv4::Regex
+attribute :ipv4_gateway, :kind_of => String, :required => true,
+          :regex => Resolv::IPv4::Regex
+attribute :adapter_name, :name_attribute => true, :kind_of => String, :required => true
